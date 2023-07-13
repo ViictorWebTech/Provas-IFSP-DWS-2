4db4d4d4b689e14c1df3bfe31e75612b56ac662f
ViictorWebTech <vwebtech777@gmail.com>
2023-07-13 12:01:34 -0300

    :tada: init: Commit inicial

---
+++ Prova 2° BIM - 2° Info/Calculadora e Gerador de Parágrafos/head.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <style>
+        .btn {
+            margin: .25em;
+        }
+        .rosa{
+            color : var(--bs-code-color);
+        }
+        body{
+            padding: 1.5em;
+        }
+
+    </style>
+</head>
+
+<body class="<?= $corfundo; ?> <?= $cortxt; ?>">
+    <div class="container">
